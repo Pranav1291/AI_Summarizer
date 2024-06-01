704b16f7abe48ce46eb98a16c1980b8fb3a5509f
Pranav Srivastva <pranav.srivastava.1291@gmail.com>
2024-06-01 19:52:18 +0530

    get issue

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -17,7 +17,7 @@ const Demo = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { data } = await getSummary({ articleUrl: article.Url });
+    const { data } = await getSummary({ articleUrl: article.url });
 
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
